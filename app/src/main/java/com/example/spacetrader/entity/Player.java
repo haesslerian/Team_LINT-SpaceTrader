bef984fa6b51118b601a516bf8ee6c4ccb7b1841
haesslerian <haesslerian@gmail.com>
2019-02-17 18:40:23 -0500

    renamed player

--- app/src/main/java/com/example/spacetrader/entity/Player.java
+++ app/src/main/java/com/example/spacetrader/entity/Player.java
@@ -13,4 +13,56 @@ public class Player {
         this.skillTrader = skillTrader;
         this.skillEngineer = skillEngineer;
     }
+
+    public Player(){
+        this("DEFAULT", Difficulty.BEGINNER, 1,1,1,1);
+    }
+
+    public int getSkillPilot() {
+        return skillPilot;
+    }
+
+    public void setSkillPilot(int skillPilot) {
+        this.skillPilot = skillPilot;
+    }
+
+    public int getSkillFighter() {
+        return skillFighter;
+    }
+
+    public void setSkillFighter(int skillFighter) {
+        this.skillFighter = skillFighter;
+    }
+
+    public int getSkillTrader() {
+        return skillTrader;
+    }
+
+    public void setSkillTrader(int skillTrader) {
+        this.skillTrader = skillTrader;
+    }
+
+    public int getSkillEngineer() {
+        return skillEngineer;
+    }
+
+    public void setSkillEngineer(int skillEngineer) {
+        this.skillEngineer = skillEngineer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 }
