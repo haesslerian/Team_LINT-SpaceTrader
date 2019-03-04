18824a096ff8a712bd82eb94af1022b7fa64dd76
haesslerian <haesslerian@gmail.com>
2019-03-04 13:36:38 -0500

    Added back some untracked files

---
+++ app/src/main/java/com/example/spacetrader/views/NextScreen.java
@@ -0,0 +1,13 @@
+package com.example.spacetrader.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NextScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_screen);
+    }
+}
